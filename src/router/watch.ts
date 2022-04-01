6cc91acd3cac7c4d27d1247cbf86e2c197dffd4d
huangyangteng <huangyangteng@ebupt.com>
2022-04-01 19:56:41 +0800

    feat:add acfun

--- src/router/watch.ts
+++ src/router/watch.ts
@@ -1,96 +1,134 @@
 import * as Router from 'koa-router'
 const router = new Router()
 import * as path from 'path'
-import query from '../db/mysql';
-import { getBBVideoId, getBBVideoSrc, getVideoUrlByRes, getBBVideoInfo } from '../tools/blibli';
-import { getRes, readFileAndParse, walkDir, getExt } from '../tools/index';
-import { generateResource, getOutput, getCodec } from '../tools/watch';
-import { WatchItemContent, WatchChildItem, WatchBBItem } from '../types/watchType';
-import { OkPacket } from '../types/index';
+import query from '../db/mysql'
+import {
+    getBBVideoId,
+    getBBVideoSrc,
+    getVideoUrlByRes,
+    getBBVideoInfo,
+} from '../tools/blibli'
+import { getRes, readFileAndParse, walkDir, getExt } from '../tools/index'
+import { generateResource, getOutput, getCodec } from '../tools/watch'
+import {
+    WatchItemContent,
+    WatchChildItem,
+    WatchBBItem,
+} from '../types/watchType'
+import { OkPacket } from '../types/index'
+import { getAcVideoInfo } from '../tools/acfun'
 
 const PROJECT_PATH = path.join(__dirname, '../data/watch.json')
 
-const DEFAULT_DIR='/Users/h/Desktop/learnvideo'
+const DEFAULT_DIR = '/Users/h/Desktop/learnvideo'
 
 // 解析blibli的视频
-router.all('/bb',async ctx=>{
-    const bid=getBBVideoId(ctx.request.body.link)
-    const onlySrc=ctx.request.body.onlySrc
+router.all('/bb', async (ctx) => {
+    const bid = getBBVideoId(ctx.request.body.link)
+    const onlySrc = ctx.request.body.onlySrc
     // 只返回视频播放的src
-    const videoSrc=await getBBVideoSrc(ctx.request.body.link)
+    const videoSrc = await getBBVideoSrc(ctx.request.body.link)
     let videoInfoRes
-    let responseBody={
-        bid: bid,src:videoSrc
+    let responseBody = {
+        bid: bid,
+        src: videoSrc,
     }
     // 如果需要返回视频列表等信息，返回视频列表
-    if(!onlySrc){
-        videoInfoRes=await getBBVideoInfo(bid)
-        responseBody=Object.assign(responseBody,{
-            ...videoInfoRes.data.data
+    if (!onlySrc) {
+        videoInfoRes = await getBBVideoInfo(bid)
+        responseBody = Object.assign(responseBody, {
+            ...videoInfoRes.data.data,
         })
     }
 
     // console.log('videoInfo',videoInfo)
-    ctx.body= getRes(2000,responseBody) 
+    ctx.body = getRes(2000, responseBody)
+})
+router.post('/acfun', async (ctx) => {
+    const res = await getAcVideoInfo(
+        ctx.request.body.link,
+        ctx.request.body.onlySrc
+    )
+    ctx.body = getRes(2000, res)
 })
 
 //curl -X POST --data-urlencode "dir=/Users/h/Desktop/learnvideo" http://localhost:9999/watch/run
-router.all('/run',async ctx=>{
-    let {dir}=ctx.request.body
-    if(!dir){
-        dir=DEFAULT_DIR
+router.all('/run', async (ctx) => {
+    let { dir } = ctx.request.body
+    if (!dir) {
+        dir = DEFAULT_DIR
     }
     try {
-        generateResource(dir,PROJECT_PATH)
-        ctx.body=getRes<string>(2000,'success')
-        
+        generateResource(dir, PROJECT_PATH)
+        ctx.body = getRes<string>(2000, 'success')
     } catch (error) {
-        ctx.body = getRes<string>(5000, '生成失败'+error)
+        ctx.body = getRes<string>(5000, '生成失败' + error)
     }
 })
 
-
-router.get('/', async ctx => {
+router.get('/', async (ctx) => {
     const data = readFileAndParse(PROJECT_PATH)
     ctx.body = getRes<WatchItemContent[]>(2000, data)
 })
-router.get('/bb-list',async ctx=>{
-    const res=await query<WatchBBItem[]>('SELECT * from `bb-video`')
-    ctx.body=getRes<WatchBBItem[]>(2000,res)
+router.get('/bb-list', async (ctx) => {
+    const res = await query<WatchBBItem[]>('SELECT * from `bb-video`')
+    ctx.body = getRes<WatchBBItem[]>(2000, res)
 })
 
-router.get('/query', async ctx => {
-    const {courseId}=ctx.query
-    console.log("courseId", courseId)
-    const data:WatchItemContent[] = readFileAndParse(PROJECT_PATH)
-    if(courseId){
-        let allCourse=data.map(item=>item.list).reduce((prev,cur)=>prev.concat(cur),[])
-        let item= allCourse.find(item=>item.id==courseId)
-        console.log("item", item)
-        if(item){
+router.get('/query', async (ctx) => {
+    const { courseId } = ctx.query
+    console.log('courseId', courseId)
+    const data: WatchItemContent[] = readFileAndParse(PROJECT_PATH)
+    if (courseId) {
+        let allCourse = data
+            .map((item) => item.list)
+            .reduce((prev, cur) => prev.concat(cur), [])
+        let item = allCourse.find((item) => item.id == courseId)
+        console.log('item', item)
+        if (item) {
             ctx.body = getRes<WatchChildItem>(2000, item)
-        }else{
-            ctx.body=getRes<string>(5000,'未找到该课程id')
+        } else {
+            ctx.body = getRes<string>(5000, '未找到该课程id')
         }
-    }else{
-        ctx.body=getRes<string>(5000,'请输入正确的查询参数')
+    } else {
+        ctx.body = getRes<string>(5000, '请输入正确的查询参数')
     }
 })
 
-router.post('/add',async ctx=>{
-    const {link,type}=ctx.request.body
-    const bid=getBBVideoId(link)
-    let res=await getBBVideoInfo(bid)
-    let {title,pic}=res.data.data
-    let req={
-        link,
-        type,
-        bid,
-        name:title,
-        poster:pic
+router.post('/add', async (ctx) => {
+    const { link, type, from = 'bb' } = ctx.request.body
+    let req
+    if (from === 'bb') {
+        const bid = getBBVideoId(link)
+        let res = await getBBVideoInfo(bid)
+        let { title, pic } = res.data.data
+        req = {
+            link,
+            type,
+            bid,
+            name: title,
+            poster: pic,
+            from: 'bb',
+        }
+    }else if(from==='acfun'){
+        const {title,pic} = await getAcVideoInfo(
+            ctx.request.body.link,
+            ctx.request.body.onlySrc
+        )
+        req = {
+            link,
+            type,
+            bid:getBBVideoId( ctx.request.body.link),
+            name: title,
+            poster: pic,
+            from: 'acfun',
+        }
     }
-    const insertInfo=await query<OkPacket>('INSERT INTO `bb-video` SET?',req)
+    const insertInfo = await query<OkPacket>('INSERT INTO `bb-video` SET?', req)
     ctx.body = getRes<number>(2000, insertInfo.insertId)
+})
+router.delete('/delete',async (ctx)=>{
+    
 })
 
 
---
+++ src/tools/acfun.ts
@@ -0,0 +1,65 @@
+import * as cheerio from 'cheerio'
+import { request } from '.';
+import { writeFile } from './index';
+import * as path from 'path';
+import * as  fs from 'fs';
+
+
+function getPages($:any) {
+    const BASE = 'https://www.acfun.cn'
+    let list:any=[]
+    $('.part li').each(function (index:number, item:any) {
+        if (index == 0) {
+            console.log(
+                index,
+                item.attribs['data-id'],
+                item.attribs['data-href'],
+                item.attribs['title']
+            )
+        }
+        list.push({
+            page: index + 1,
+            part: item.attribs['title'],
+            id: item.attribs['data-id'],
+            cid: item.attribs['data-id'],
+            link: BASE + item.attribs['data-href'],
+            isAcfun:true
+        })
+    })
+    return list
+}
+
+function getVideoInfo(str:string) {
+    //[version:'',adaptationSet:[{id:0,duration:2000,representation:[{id:1,url:''},{id:2,url:''}]}]]
+    var reg = /ksplayJson":"(\{.*\})","ksPlayJsonHevc/gim
+    let res = reg.exec(str)
+    // console.log( res[1][0])
+    // console.log(res.length,JSON.parse(JSON.parse(`"${res[1]}"`)) )
+    const videoInfo=JSON.parse(JSON.parse(`"${res[1]}"`))
+    return videoInfo.adaptationSet[0].representation[0].url
+}
+function getTitle($:any){
+    return $('h1.title').text()
+}
+function getCover(str:string){
+    let reg=/coverUrl":\s*"(https?:\/\/[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|])"/gi
+    return reg.exec(str)[1]
+}
+
+export const getAcVideoInfo=async(link:string,onlySrc:boolean|undefined)=>{
+    const api=`
+        curl ${link}
+    `
+    const res=await request(api)
+    const FILES_PATH = path.join(__dirname, '../data/test.json')
+    fs.writeFileSync(FILES_PATH, res.data)
+    const $ = cheerio.load(res.data)
+    const str=$.html()
+    return {
+        title:getTitle($),
+        pic:getCover(str),
+        pages:getPages($),
+        src:getVideoInfo(str)
+    }
+
+}
