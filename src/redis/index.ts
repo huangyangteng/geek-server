113947de2ed9cf27b74e63997abe5f94c1196e84
max <hyangteng@gmail.com>
2024-05-21 17:24:18 +0800

    feat:redis add error handle

--- src/redis/index.ts
+++ src/redis/index.ts
@@ -1,9 +1,13 @@
-import { createClient } from 'redis';
+import { createClient } from 'redis'
 import config from './config'
 const client = createClient(config)
-client.connect();
-  
-export const redisClient=client
+client.connect()
+client.on('error', (err) => {
+    console.error('Redis error:', err)
+})
 
+client.on('connect', () => {
+    console.log('Connected to Redis')
+})
 
-
+export const redisClient = client
